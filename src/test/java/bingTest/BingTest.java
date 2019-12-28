e207389e5f3f38c6ae505f2365970d3cec81e3f7
kandhavel <a.r.u.andavel@gmail.com>
2019-12-28 18:29:50 +0530

    spprint2.0 code1

--- src/test/java/bingTest/BingTest.java
+++ src/test/java/bingTest/BingTest.java
@@ -1,5 +1,23 @@
 package bingTest;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BingTest {
+
+	
+	 public void logintest() throws IOException{
+		 WebDriver wd=new FirefoxDriver();
+		 
+		 wd.get("https://www.google.com/");
+		 File src = ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(src, new File("\\Selenium workspace\\GridTest\\Screenshot"));
+	 }
 
 }
